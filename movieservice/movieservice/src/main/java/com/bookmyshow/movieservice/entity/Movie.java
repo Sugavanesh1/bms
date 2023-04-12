e559da861f0edb8e95b328732a3c0e562d2ed66f
Sugavanesh <meetmeoften@gmail.com>
2023-04-12 21:02:52 +0530

    minor

--- movieservice/movieservice/src/main/java/com/bookmyshow/movieservice/entity/Movie.java
+++ movieservice/movieservice/src/main/java/com/bookmyshow/movieservice/entity/Movie.java
@@ -5,20 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Movie {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     public long movieId; // primaryKey
     public String name;
     public boolean isActive;
